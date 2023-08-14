bb7043f4b3e4c5b6b7902cbade99ba99e48e24c5
Harriet Oteng <harriet.oteng@gmail.com>
2023-08-14 18:40:44 +0000

    Add home page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  # get 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   namespace :api do
     resources :messages, only: [:index]
   end
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
